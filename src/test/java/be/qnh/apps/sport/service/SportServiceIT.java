307cf25301460274a694befc3c5d6308559095ab
Raymond Loman <raymond@carpago.nl>
2018-02-19 12:50:40 +0100

    Add test for testing the failing delete

--- src/test/java/be/qnh/apps/sport/service/SportServiceIT.java
+++ src/test/java/be/qnh/apps/sport/service/SportServiceIT.java
@@ -48,4 +48,35 @@ public class SportServiceIT {
       
       Assert.assertNull(this.sportService.findById(id));
    }
+   
+   @Test
+   public void testDeletingNonExisting() {
+      
+      Sport nieuwe = new Sport();
+      nieuwe.setMixed(true);
+      nieuwe.setName("Schaken");
+      
+      Sport sportCreated = this.sportService.insert(nieuwe);
+      
+      Assert.assertTrue(sportCreated.getId() != 0);
+      
+      long id = sportCreated.getId();
+      
+      Sport opgehaald = this.sportService.findById(id);
+      
+      Assert.assertEquals(nieuwe.getName(), opgehaald.getName());
+      
+      opgehaald.setName("Schaken en Dammen");
+      
+      Sport opgehaaldNaSaven = this.sportService.update(id, opgehaald);
+      
+      Assert.assertEquals("Schaken en Dammen", opgehaaldNaSaven.getName());
+      
+      this.sportService.deleteById(id);
+      
+      Assert.assertNull(this.sportService.findById(id));
+      
+      this.sportService.deleteById(id);
+      
+   }
 }
