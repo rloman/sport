6f8936d360a1f608b5522ec384bed7264eaa04d8
Raymond Loman <raymond@carpago.nl>
2018-05-09 13:14:38 +0200

    Reverst some unwanted

--- src/main/java/be/qnh/apps/sport/service/SportService.java
+++ src/main/java/be/qnh/apps/sport/service/SportService.java
@@ -63,12 +63,6 @@ public class SportService {
       
       Sport victim = this.repo.findOne(id);
 
-      List<Integer> someNumbers = Arrays.asList(1,2,3,4,5,6,7);
-
-      someNumbers.stream().filter(n -> n%2 ==0).forEach(n -> {
-         System.out.println(n);
-      });
-      
       if(victim != null) {
          victim.setName(sportIn.getName());
          victim.setMixed(sportIn.isMixed());
